a2da6d877d3da0b70fa542a1168dd912c2f1a507
Elias Forte <eliasforte.dev@gmail.com>
2022-04-20 15:21:05 -0300

    [feat]: create component selector

---
+++ src/components/Selector.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label>
+      <span>{{ title }}</span>
+      <select>
+        <option selected value="item">Selecione o item</option>
+        <option v-for="(option, index) in options" :key="index">
+          {{ option }}
+        </option>
+      </select>
+    </label>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SelectorForm',
+  props: {
+    title: String,
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+}
+
+</script>
