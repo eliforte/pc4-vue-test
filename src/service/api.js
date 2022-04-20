fa07e5a70ebb8ae30f93ffea6a1fee940028118e
Elias Forte <eliasforte.dev@gmail.com>
2022-04-20 20:16:09 -0300

    [fix]: request function

--- src/service/api.js
+++ src/service/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const getUsers = () => {
   return axios.get("https://private-5b8666-testefrontendpc4.apiary-mock.com/users")
-    .then(response => (
-      localStorage.setItem("users", JSON.stringify(response.data[0].users))
-    ))
+    .then(response => {
+      !localStorage.getItem('users') ? localStorage.setItem('users', JSON.stringify(response.data[0].users)) : null; 
+    })
     .catch(error => {
     console.log(error)
   })
