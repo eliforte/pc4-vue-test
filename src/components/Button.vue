e8f8bba04ec2a57eced58518007bbf2a6690e618
Elias Forte <eliasforte.dev@gmail.com>
2022-04-20 15:19:52 -0300

    [feat]: create componente button

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button 
+      v-if="typeButton"
+      @click="$router.push('/create')"
+    >
+      Novo
+    </button>
+    <button
+      v-else
+    >
+      Salvar
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ButtonForm',
+  props: {
+    typeButton: {
+      type: Boolean,
+      required: true
+    }
+  },
+}
+</script>
