f2be36b3018650bf8f6c6ad3e50147da1be0d097
Elias Forte <eliasforte.dev@gmail.com>
2022-04-20 20:12:15 -0300

    [fix]: remove global style

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,9 @@ export default {
 </script>
 
 <style lang="sass">
-*
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  display: flex;
-
-
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto', sans-serif
+  box-sizing: border-box
+  display: flex
 
 </style>
