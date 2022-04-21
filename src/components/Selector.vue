d9427b9c4b7a8fb17fd10e4bc94cb4e3f5222026
Elias Forte <eliasforte.dev@gmail.com>
2022-04-20 21:45:23 -0300

    [feat]: add style selections

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <label>
-      <span>{{ title }}</span>
-      <select>
-        <option selected value="item">Selecione o item</option>
-        <option v-for="(option, index) in options" :key="index">
-          {{ option }}
-        </option>
-      </select>
-    </label>
-  </div>
+  <label class="label-select">
+    <span class="title-select">{{ title }}</span>
+    <select v-model="select">
+      <option selected disabled value="Selecione o item">Selecione o item</option>
+      <option v-for="(option, index) in options" :key="index">
+        {{ option }}
+      </option>
+    </select>
+  </label>
 </template>
 
 <script>
@@ -23,6 +21,43 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      select: 'Selecione o item',
+    }
+  }
 }
 
 </script>
+
+<style lang="sass">
+@import '../sass/_mixins';
+
+.label-select
+  display: flex
+  flex-direction: column
+  margin-bottom: 1rem
+  width: 100%
+
+  .title-select
+    font-size: 1.2rem
+    margin: 0 0 1rem 0
+
+  select
+    border: solid 1px #C4C4C4
+    background-color: #fff
+    font-size: 1.2rem
+    padding: .7rem 0
+    width: 100%
+    border-radius: 6px
+
+    &:active
+      width: 100%
+      border-radius: 4px
+
+
+.title-select
+  font-weight: bold
+  font-size: 1.2rem
+  margin: 0 2rem 1rem 0
+</style>
