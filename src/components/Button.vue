1a5f02614e37673be55780a0d0d090e91c396ab2
Elias Forte <eliasforte.dev@gmail.com>
2022-04-20 21:43:56 -0300

    [feat]: add style button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <button 
+    <button
+      class="btn-create"
       v-if="typeButton"
       @click="$router.push('/create')"
     >
       Novo
     </button>
     <button
+      class="btn-create"
       v-else
       @click="$router.push('/')"
     >
@@ -27,3 +29,24 @@ export default {
   },
 }
 </script>
+
+<style lang="sass">
+@import '../sass/_mixins';
+
+@include mobile-version
+  .btn-create
+    font-size: 1.2rem
+    margin: 4rem 0 1rem 0
+    padding: 1rem 0
+    width: 100%
+    background-color: #4F46E5
+    color: #fff
+    border: solid 1px #fff
+    border-radius: 4px
+
+    &:hover
+      background-color: #fff
+      color: #4F46E5
+      border: solid 1px #4F46E5
+
+</style>
