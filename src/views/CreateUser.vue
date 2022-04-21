676f3ccd4f4451c1f12b31b98c66fe69fff4f259
Elias Forte <eliasforte.dev@gmail.com>
2022-04-21 04:19:17 -0300

    [feat]: add CreateUser style

--- src/views/CreateUser.vue
+++ src/views/CreateUser.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-container">
     <label class="user-container">
-      <span>Nome</span>
+      <span>Foto</span>
       <div class="image-container">
         <img src="test" alt="teste">
         <button>Modificar</button>
@@ -10,15 +10,32 @@
     <form>
         <label for="name">
           <span>Nome</span>
-          <input v-model="name" type="text" name="name" placeholder="Digite o nome">    
+          <input
+            @blur="name.length <= 3 ? erroName = 'erro' : erroName = 'ok'"
+            v-model="name"
+            type="text" 
+            name="name"
+            :class="erroName === 'erro' ? 'input-form erro' : 'input-form'"
+            placeholder="Digite o nome"
+          >
+          <h5 class="erro-text" v-if="erroName === 'erro'">O campo deve conter 3 caracteres ou mais</h5>
         </label>
         <label for="email">
           <span>Email</span>
-          <input v-model="email" type="text" name="email" placeholder="me@example.com">
+          <input
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            v-model="email"
+            type="text"
+            name="email" 
+            placeholder="me@example.com"
+            class="input-form"
+          >
         </label>
         <SelectorForm :title="title = 'Setor'" :options="departments"/>
         <SelectorForm :title="title = 'Cargo'" :options="occupations"/>
         <SelectorForm :title="title = 'Função'" :options="roles"/>
+        <img class="switch-button" v-if="erroName !== 'ok'" src="../assets/inative.svg" alt="switch-image-off"/>
+        <img class="switch-button" v-else src="../assets/active.svg" alt="switch-image-on"/>
         <ButtonForm :typeButton="home"/>
     </form>
   </div>
@@ -33,6 +50,12 @@ export default {
   components: {
     SelectorForm,
     ButtonForm,
+  },
+  methods: {
+    validEmail: function (email) {
+      var pattern = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+      return pattern.test(email);
+    },
   },
   data() {
     return {
@@ -62,6 +85,8 @@ export default {
       home: false,
       name: '',
       email: '',
+      erroName: '',
+      url: '',
     }
   },
 }
@@ -75,11 +100,17 @@ export default {
     display: flex
     flex-direction: column
     width: 100%
-    margin: 1.2rem
+    height: 100%
+    margin: 3rem .5rem 0 0
+    justify-items: center
+
+  label
+    margin: .5rem 0
 
   .user-container
     display: flex
     flex-direction: column
+    margin: 0 0 3rem 0
     
     span
       font-size: 1.2rem
@@ -103,8 +134,62 @@ export default {
         font-size: 1.2rem
         cursor: pointer
       
-
   form
     display: flex
     flex-direction: column
+
+    @include mobile-version
+      width: 100%
+
+      span
+        font-size: 1.2rem
+        font-weight: bold
+
+      .input-form
+        font-size: 1.2rem
+        margin: .5rem 0
+        border: solid 1px #C4C4C4
+        border-radius: 6px
+        width: 100%
+        height: 2.5rem
+        
+      
+      .erro
+        border: solid 1px #E42C17
+        color: #E42C17
+      
+        &::placeholder
+          color: #E42C17
+
+      .erro-text
+        margin: .2rem 0 .4rem 0
+        color: #E42C17
+        position: static
+        animation: showText 500ms normal
+
+        @keyframes showText 
+          to
+            transform: 
+            opacity: 0.5
+          from
+            opacity: 1
+        
+
+      .switch-button
+        width: 5rem
+        height: 3rem
+        margin: 10px 0
+        cursor: pointer
+        animation: changeButton 300ms linear alternate-reverse
+        
+        @keyframes changeButton 
+          to 
+            transform: translateZ(-1px)
+            transform: rotateY(20deg)
+            opacity: 1
+          from
+            transform: translateZ(0)
+            transform: rotateY(10deg)
+            opacity: 0.70
+        
 </style>
