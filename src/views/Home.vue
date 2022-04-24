0793902f9519d20658cfba49371fce5bcef52a2e
Elias Forte <eliasforte.dev@gmail.com>
2022-04-24 19:14:36 -0300

    [style]: new css code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <table >
       <thead class="header-table">
         <tr v-for="(title, index) in titles" :key="index">
-          <th>{{ title }}</th>
+          <th :class="title.class">{{ title.name }}</th>
         </tr>
       </thead>
       <tbody>
@@ -44,7 +44,19 @@ export default {
   },
   data() {
     return {
-      titles: ['Nome', 'Cargo', 'Status', 'Função'],
+      titles: [{
+        name: 'NOME',
+        class: 'th-name',
+      }, {
+        name: 'CARGO',
+        class: 'th-ocuppation',
+      }, {
+        name: 'STATUS',
+        class: 'th-status',
+      }, {
+        name: 'FUNÇÃO',
+        class: 'th-role',
+      }],
       loading: false,
       homeButton: true,
       screen: 0,
@@ -70,225 +82,184 @@ export default {
 <style lang="sass">
 @import '../sass/_mixins';
 
-// .home-container
-//   height: 100%
-//   width: 100%
+.home-container
+  height: 100%
+  width: 100%
 
-//   table
-//     margin: 97px 217px
-//     @include mobile-version
-//       margin: auto auto
+  table
+    padding: 30px 0
+    border: 1px solid #F9FAFB
+    border-radius: 10px
+    margin: 97px 217px
+    background-color: #fff
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.25);
 
-// .header-table
-//   width: 67%
-//   margin-left: 10%
-//   height: 50px
-//   display: flex
-//   align-items: center
-//   justify-content: space-evenly
+    @include mobile-version
+      margin: 0
 
-//   th
-//     grid-template-columns: repeat(5, 1fr)
-//     width: 150px
+.header-table
+  color: #6B7280
+  grid-template-columns: repeat(4, 1fr)
+  margin: 0 25px
+  width: 90%
+  height: 50px
+  display: grid
+  align-items: center
+  justify-content: space-around
 
-//   @include mobile-version
-//     display: none
+  tr
+    width: 70px
 
-// .tr-body-container
-//   margin: 0 10%
-//   align-items: center
-//   width: 80%
-//   display: grid
-//   grid-template-columns: repeat(5, 1fr)
-//   border: solid 1px #E5E7EB
-  
-//   @include mobile-version
-//     margin: 0
-//     width: 100%
-//     height: 100%
+  @include mobile-version
+    display: none
+    width: 100%
 
-//   td
-//     margin: 0
-//     height: 70px
-//     display: flex
-  
-// .box-profile
-//   padding: 0 40px
-//   width: 200px
-//   height: 70px
-//   display: flex
-//   align-items: center
-
-//   img
-//     width: 40px
-//     height: 40px
-//     border-radius: 50%
-  
-//   div
-//     display: flex
-//     flex-direction: column
-//     flex-wrap: wrap
-//     justify-content: center
-
-//     h3
-//       margin: auto 10px
-//       color: #111827
-//       font-size: 19px
+  .th-role
     
-//     p
-//       color: #6B7280
-//       margin: .1rem 10px
-//       font-size: 15px
+
+  th
+    grid-template-columns: repeat(3, 1fr)
+
+.tr-body-container
+  align-items: center
+  border: solid 1px #E5E7EB
+  display: grid
+  grid-template-columns: repeat(5, 1fr)
+  justify-items: center
+  width: 100%
+  
+  @include mobile-version
+    grid-template-columns: repeat(3, 1fr)
+    margin: 0
+    width: 100%
+    height: 100%
+
+  td
+    margin: 20px 0
+    height: 70px
+    width: 100%
+    display: flex
+
+    @include mobile-version
+      margin: 20px 0
+      height: 100%
+      width: 100%
+
+.box-profile
+  padding-left: 40px
+  width: 200px
+  height: 70px
+  display: flex
+  align-items: center
+  grid-column-start: 1
+  grid-column-end: 2
+
+  @include mobile-version
+    padding-left: 30px
+    grid-column-end: 4
+
+  img
+    width: 40px
+    height: 40px
+    border-radius: 50%
+    
+    @include mobile-version
+      width: 170px
+      height: 120px
+
+  div
+    display: flex
+    flex-direction: column
+    flex-wrap: wrap
+    justify-content: center
+
+    @include mobile-version
+      flex-direction: row
+
+    h3
+      margin: auto 10px
+      color: #111827
+      font-size: 19px
+    
+    p
+      color: #6B7280
+      margin: .1rem 10px
+      font-size: 15px
       
 
-// .box-ocuppation
-//   display: flex
-//   flex-direction: column
-//   align-items: flex-start;
-//   flex-wrap: wrap
-//   justify-content: center;
+.box-ocuppation
+  align-items: flex-start;
+  display: flex
+  flex-direction: column
+  justify-content: center
+  grid-column-start: 2
+  grid-column-end: 3
 
-//   h3
-//     margin: 0
-//     color: #111827
-//     font-size: 19px
-//   p
-//     margin: 0
+  @include mobile-version
+    grid-column-start: 1
+    flex-direction: row
+    flex-wrap: wrap
+    padding-left: 100px
+    align-items: center
+    align-content: center
+      
+  h3
+    margin: 0
+    color: #111827
+    font-size: 19px
 
-// span
-//   margin: 40px
-//   align-items: center
-//   padding: 10px 14px 10px 11px
+    @include mobile-version
+      margin: auto 10px
+  p
+    margin: auto 10px
+    margin: 0
 
-// .status
-//   height: 16px
-//   width: 40px
-//   border-radius: 4px
-//   font-weight: 16.41px
+span
+  margin: 15px 60px 
+  align-items: center
+  padding: 10px 14px 10px 11px
 
-// .active
-//   color: #065F46
-//   background-color: #D1FAE5
+.status
+  height: 16px
+  width: 40px
+  border-radius: 4px
+  font-weight: 16.41px
+
+.active
+  padding: 10px 5px 10px 11px
+  color: #065F46
+  background-color: #D1FAE5
   
-// .inativo
-//   color: #991B1B
-//   background-color: #FFE2E3
+.inativo
+  padding: 10px 14px 10px 11px
+  color: #991B1B
+  background-color: #FFE2E3
 
-// .end
-//   align-self: center
-//   size: 14px
+.end
+  justify-content: space-around
+  align-self: center
+  size: 14px
 
-// .role
-//   width: 40px
-//   align-items: center
-//   color: #6B7280
-//   font-weight: 16.41px
+  @include mobile-version
+    grid-row-start: 3
 
-// .edit-btn
-//   width: 40px
-//   align-items: center
-//   color: #4F46E5
-//   font-weight: 16.41px
-//   size: 14px
+.role
+  width: 40px
+  align-items: center
+  color: #6B7280
+  font-weight: 16.41px
 
-//   @include mobile-version
-//     grid-template-columns: repeat(3, 1fr)
-//     padding: 10px 0px  
+  @include mobile-version
+    grid-row-start: 3
 
-//     .box-profile
-//       margin: 5px auto
-//       grid-column-start: 1
-//       grid-column-end: 4
-//       grid-row-start: 1
+.edit-btn
+  width: 40px
+  align-items: center
+  color: #4F46E5
+  font-weight: 16.41px
+  size: 14px
 
-//       img
-//         width: 150px
-//         height: 110px
-//         padding: 0 .8rem
-
-//       div
-//         flex-direction: row
-
-//         h3
-//           margin: auto .2rem
-        
-//         p
-//           margin: auto .2rem
-//           color: #6B7280
-        
-//         .label-mobile
-//           color: #656565
-
-
-//     .box-ocuppation
-//       display: grid
-//       grid-template-columns: repeat(2, 1fr)
-//       justify-content: center
-//       align-items: center
-//       justify-items: center
-//       height: 120px
-//       width: 175px
-//       animation: showContainer 200ms linear alternate-reverse
-
-//       @keyframes showContainer 
-//         to
-//           transform: translateY(-20px)
-//           opacity: 0.1
-//         from
-//           transform: translateY(0)
-//           opacity: 1
-
-//       h3
-//         font-size: 1.1rem
-//         font-weight: bold
-//         color: #111827
-//         justify-items: flex-end
-//         margin: 0
-//       p
-//         grid-column-start: 1
-//         grid-column-end: 4
-//         font-size: 1rem
-//         color: #6B7280
-//         margin: 0
-
-//       .label-mobile
-//         color: #656565
-
-//     .status
-//       border-radius: 4px
-//       padding: 10px 10px
-//       margin: 10px 12px
-//       font-weight: bold
-
-//     .active
-//       color: #065F46
-//       background-color: #D1FAE5
-    
-//     .inativo
-//       color: #991B1B
-//       background-color: #FFE2E3
-
-//     .end
-//       display: flex
-//       justify-content: center
-//       animation: showContainer 200ms linear alternate-reverse
-
-//     .role
-//       color: #6B7280
-//       padding: 10px 10px
-//       margin: 10px 12px
-
-//     .edit-btn
-//       color: #4F46E5
-//       background: none
-//       border: solid 1px #4F46E5
-//       border-radius: 4px
-//       grid-column-start: 1
-//       grid-column-end: 4
-//       padding: 1rem 0
-
-//       &:hover
-//         background: #4F46E5
-//         color: #fff
+  @include mobile-version
+    grid-row-start: 3
 
 </style>
